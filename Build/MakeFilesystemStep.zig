6dd94d6c46275232e1d7ed13f7eed381768554da
Binary Craft <keep.it.sns@gmail.com>
2023-10-05 15:56:05 +0800

    upgrade to zig master

--- Build/MakeFilesystemStep.zig
+++ Build/MakeFilesystemStep.zig
@@ -3,6 +3,12 @@ const os = std.os;
 const mem = std.mem;
 const math = std.math;
 const assert = std.debug.assert;
+const MakeFilesystemStep = @This();
+
+const Build = std.Build;
+const InstallDir = Build.InstallDir;
+const CompileStep = Build.CompileStep;
+const Step = Build.Step;
 
 const fs = @import("fs.zig");
 const stat = @import("stat.zig");
@@ -23,8 +29,62 @@ var freeinode: u32 = 1;
 var freeblock: u32 = undefined;
 var file: std.fs.File = undefined;
 
-pub fn main() !void {
-    const argv = os.argv;
+step: Step,
+artifacts: std.ArrayList(*CompileStep),
+dest_dir: InstallDir,
+dest_filename: []const u8,
+output_file: std.Build.GeneratedFile,
+
+pub fn create(
+    owner: *Build,
+    artifacts: std.ArrayList(*CompileStep),
+    dest_filename: []const u8,
+) *MakeFilesystemStep {
+    const self = owner.allocator.create(MakeFilesystemStep) catch @panic("OOM");
+    self.* = MakeFilesystemStep{
+        .step = Step.init(.{
+            .id = .custom,
+            .owner = owner,
+            .name = owner.fmt("make filesystem image {s}", .{dest_filename}),
+            .makeFn = make,
+        }),
+        .artifacts = artifacts,
+        .dest_dir = .bin,
+        .dest_filename = dest_filename,
+        .output_file = std.build.GeneratedFile{ .step = &self.step },
+    };
+
+    for (artifacts.items) |artifact| {
+        self.step.dependOn(&artifact.step);
+    }
+
+    owner.pushInstalledFile(self.dest_dir, dest_filename);
+    return self;
+}
+
+pub fn getOutputSource(self: *const MakeFilesystemStep) std.Build.FileSource {
+    return std.Build.FileSource{ .generated = &self.output_file };
+}
+
+fn make(step: *Step, prog_node: *std.Progress.Node) !void {
+    _ = prog_node;
+    const self = @fieldParentPtr(MakeFilesystemStep, "step", step);
+    const b = self.step.owner;
+
+    var full_src_paths = std.ArrayList([]const u8).init(b.allocator);
+    try full_src_paths.append("README");
+    for (self.artifacts.items) |artifact| {
+        try full_src_paths.append(artifact.getOutputSource().getPath(b));
+    }
+
+    const full_dest_path = b.getInstallPath(self.dest_dir, self.dest_filename);
+    self.output_file.path = full_dest_path;
+
+    std.fs.cwd().makePath(b.getInstallPath(self.dest_dir, "")) catch unreachable;
+
+    var dir = try std.fs.cwd().openDir(b.getInstallPath(self.dest_dir, ""), .{});
+    defer dir.close();
+
     var de: fs.Dirent = undefined;
     var buf: [fs.BSIZE]u8 = undefined;
     var din: fs.Dinode = undefined;
@@ -32,8 +92,8 @@ pub fn main() !void {
     assert(fs.BSIZE % @sizeOf(fs.Dinode) == 0);
     assert(fs.BSIZE % @sizeOf(fs.Dirent) == 0);
 
-    file = try std.fs.cwd().createFile(
-        mem.span(argv[1]),
+    file = try dir.createFile(
+        self.dest_filename,
         .{
             .read = true,
             .truncate = true,
@@ -63,25 +123,25 @@ pub fn main() !void {
         try wsect(i, &zeroes);
     }
 
-    mem.set(u8, &buf, 0);
-    mem.copy(u8, &buf, &@bitCast([@sizeOf(@TypeOf(sb))]u8, sb));
+    @memset(&buf, 0);
+    mem.copy(u8, &buf, mem.asBytes(&sb));
     try wsect(1, &buf);
 
-    const rootino = @intCast(u16, try ialloc(.dir));
+    const rootino = @as(u16, @intCast(try ialloc(.dir)));
     std.debug.assert(rootino == fs.ROOTINO);
 
-    mem.set(u8, mem.asBytes(&de), 0);
+    @memset(mem.asBytes(&de), 0);
     de.inum = mem.readVarInt(u16, mem.asBytes(&rootino), .Little);
     mem.copy(u8, &de.name, ".");
     try iappend(@as(u32, rootino), mem.asBytes(&de));
 
-    mem.set(u8, mem.asBytes(&de), 0);
+    @memset(mem.asBytes(&de), 0);
     de.inum = mem.readVarInt(u16, mem.asBytes(&rootino), .Little);
     mem.copy(u8, &de.name, "..");
     try iappend(@as(u32, rootino), mem.asBytes(&de));
 
-    for (argv[2..]) |arg| {
-        var path = mem.span(arg);
+    for (full_src_paths.items) |full_src_path| {
+        var path = full_src_path;
         var shortname = std.fs.path.basename(path);
 
         const bin = try std.fs.cwd().openFile(path, .{});
@@ -91,8 +151,8 @@ pub fn main() !void {
             shortname = shortname[1..];
         }
 
-        var inum = @intCast(u16, try ialloc(.file));
-        mem.set(u8, mem.asBytes(&de), 0);
+        var inum = @as(u16, @intCast(try ialloc(.file)));
+        @memset(mem.asBytes(&de), 0);
         de.inum = mem.readVarInt(u16, mem.asBytes(&inum), .Little);
         mem.copy(u8, &de.name, shortname);
         try iappend(@as(u32, rootino), mem.asBytes(&de));
@@ -114,21 +174,21 @@ pub fn main() !void {
     try balloc(@as(usize, freeblock));
 }
 
-pub fn wsect(sec: usize, buf: []const u8) !void {
+fn wsect(sec: usize, buf: []const u8) !void {
     std.debug.assert(buf.len == fs.BSIZE);
     try file.seekTo(sec * fs.BSIZE);
     const num_write = try file.writer().write(buf);
     std.debug.assert(num_write == buf.len);
 }
 
-pub fn rsect(sec: usize, buf: []u8) !void {
+fn rsect(sec: usize, buf: []u8) !void {
     std.debug.assert(buf.len == fs.BSIZE);
     try file.seekTo(sec * fs.BSIZE);
     const num_read = try file.reader().read(buf);
     std.debug.assert(num_read == buf.len);
 }
 
-pub fn winode(inum: u32, ip: *fs.Dinode) !void {
+fn winode(inum: u32, ip: *fs.Dinode) !void {
     var buf: [fs.BSIZE]u8 = undefined;
     var bn = sb.IBLOCK(inum);
     try rsect(bn, &buf);
@@ -137,21 +197,21 @@ pub fn winode(inum: u32, ip: *fs.Dinode) !void {
     try wsect(bn, &buf);
 }
 
-pub fn rinode(inum: u32, ip: *fs.Dinode) !void {
+fn rinode(inum: u32, ip: *fs.Dinode) !void {
     var buf: [fs.BSIZE]u8 = undefined;
     var bn = sb.IBLOCK(inum);
     try rsect(bn, &buf);
     var dip = buf[(inum % fs.IPB) * @sizeOf(fs.Dinode) ..];
-    ip.* = @bitCast(fs.Dinode, dip[0..@sizeOf(fs.Dinode)].*);
+    mem.copy(u8, mem.asBytes(ip), dip[0..@sizeOf(fs.Dinode)]);
 }
 
-pub fn ialloc(@"type": stat.FileType) !u32 {
+fn ialloc(@"type": stat.FileType) !u32 {
     var inum = freeinode;
     defer freeinode += 1;
 
     var din: fs.Dinode = undefined;
-    mem.set(u8, mem.asBytes(&din), 0);
-    var din_bytes = mem.toBytes(@enumToInt(@"type"));
+    @memset(mem.asBytes(&din), 0);
+    var din_bytes = mem.toBytes(@intFromEnum(@"type"));
     din.type = mem.readVarInt(i16, &din_bytes, .Little);
     din.nlink = mem.readVarInt(i16, &[_]u8{1}, .Little);
     din.size = mem.readVarInt(u32, &[_]u8{0}, .Little);
@@ -160,25 +220,24 @@ pub fn ialloc(@"type": stat.FileType) !u32 {
     return inum;
 }
 
-pub fn balloc(used: usize) !void {
+fn balloc(used: usize) !void {
     var buf: [fs.BSIZE]u8 = undefined;
-    mem.set(u8, &buf, 0);
+    @memset(&buf, 0);
 
     std.debug.assert(used < fs.BSIZE * 8);
 
-    for (buf[0..used]) |_, i| {
-        buf[i / 8] |= @as(u8, 0x1) << @intCast(u3, (i % 8));
+    for (0..used) |i| {
+        buf[i / 8] |= @as(u8, 0x1) << @as(u3, @intCast((i % 8)));
     }
 
     try wsect(sb.bmapstart, &buf);
 }
 
-pub fn iappend(inum: u32, data: []const u8) !void {
+fn iappend(inum: u32, data: []const u8) !void {
     var din: fs.Dinode = undefined;
     var buf: [fs.BSIZE]u8 = undefined;
     var n: usize = data.len;
     var n1: usize = undefined;
-    var x: usize = undefined;
     var idx: usize = 0;
     var indirect: [fs.NINDIRECT]u32 = undefined;
 
@@ -187,19 +246,19 @@ pub fn iappend(inum: u32, data: []const u8) !void {
 
     while (n > 0) : ({
         n -= n1;
-        off += @intCast(u32, n1);
+        off += @as(u32, @intCast(n1));
         idx += n1;
     }) {
         var fbn = off / fs.BSIZE;
         std.debug.assert(fbn < fs.MAXFILE);
-        if (fbn < fs.NDIRECT) {
+        var x = if (fbn < fs.NDIRECT) blk: {
             if (mem.readVarInt(u32, mem.asBytes(&din.addrs[fbn]), .Little) == 0) {
                 var fblk = mem.readVarInt(u32, mem.asBytes(&freeblock), .Little);
                 defer freeblock += 1;
                 din.addrs[fbn] = fblk;
             }
-            x = mem.readVarInt(usize, mem.asBytes(&din.addrs[fbn]), .Little);
-        } else {
+            break :blk mem.readVarInt(usize, mem.asBytes(&din.addrs[fbn]), .Little);
+        } else blk: {
             if (mem.readVarInt(u32, mem.asBytes(&din.addrs[fs.NDIRECT]), .Little) == 0) {
                 var fblk = mem.readVarInt(u32, mem.asBytes(&freeblock), .Little);
                 defer freeblock += 1;
@@ -213,9 +272,9 @@ pub fn iappend(inum: u32, data: []const u8) !void {
                 indirect[fbn - fs.NDIRECT] = fblk;
                 try wsect(num, mem.sliceAsBytes(&indirect));
             }
-            x = mem.readVarInt(usize, mem.asBytes(&indirect[fbn - fs.NDIRECT]), .Little);
-        }
-        n1 = math.min(n, (fbn + 1) * fs.BSIZE - off);
+            break :blk mem.readVarInt(usize, mem.asBytes(&indirect[fbn - fs.NDIRECT]), .Little);
+        };
+        n1 = @min(n, (fbn + 1) * fs.BSIZE - off);
         try rsect(x, &buf);
 
         mem.copy(u8, buf[off - (fbn * fs.BSIZE) ..], data[idx..][0..n1]);
--- build.zig
+++ build.zig
@@ -1,9 +1,107 @@
 const std = @import("std");
 const mem = std.mem;
-const CompileStep = std.build.CompileStep;
-const InstallFileStep = std.build.InstallFileStep;
-const MakeFilesystemStep = @import("mkfs/MakeFilesystemStep.zig");
-const QemuRunStep = @import("mkfs/QemuRunStep.zig");
+const RunStep = std.Build.RunStep;
+const CompileStep = std.Build.CompileStep;
+const InstallFileStep = std.Build.InstallFileStep;
+const MakeFilesystemStep = @import("Build/MakeFilesystemStep.zig");
+const SyscallGenStep = @import("Build/SyscallGenStep.zig");
+const QemuRunStep = @import("Build/QemuRunStep.zig");
+
+const kernel_src = [_][]const u8{
+    "src/kernel/entry.S", // Very first boot instructions.
+    "src/kernel/start.c", // Early machine-mode boot code.
+    "src/kernel/console.c", // Connect to the user keyboard and screen.
+    "src/kernel/printf.c", // Formatted output to the console.
+    "src/kernel/uart.c", // Serial-port console device driver.
+    "src/kernel/kalloc.c", // Physical page allocator.
+    "src/kernel/spinlock.c", // Locks that don’t yield the CPU.
+    "src/kernel/string.c", // C string and byte-array library.
+    "src/kernel/main.c", // Control initialization of other modules during boot.
+    "src/kernel/vm.c", // Manage page tables and address spaces.
+    "src/kernel/proc.c", // Processes and scheduling.
+    "src/kernel/swtch.S", // Thread switching.
+    "src/kernel/trampoline.S", // Assembly code to switch between user and kernel.
+    "src/kernel/trap.c", // C code to handle and return from traps and interrupts.
+    "src/kernel/syscall.c", // Dispatch system calls to handling function.
+    "src/kernel/sysproc.c", // Process-related system calls.
+    "src/kernel/bio.c", // Disk block cache for the file system.
+    "src/kernel/fs.c", // File system.
+    "src/kernel/log.c", // File system logging and crash recovery.
+    "src/kernel/sleeplock.c", // Locks that yield the CPU.
+    "src/kernel/file.c", // File descriptor support.
+    "src/kernel/pipe.c", // Pipes.
+    "src/kernel/exec.c", // exec() system call.
+    "src/kernel/sysfile.c", // File-related system calls.
+    "src/kernel/kernelvec.S", // Handle traps from kernel, and timer interrupts.
+    "src/kernel/plic.c", // RISC-V interrupt controller.
+    "src/kernel/virtio_disk.c", // Disk device driver.
+};
+
+const cflags = [_][]const u8{
+    "-Wall",
+    "-Werror",
+    "-Wno-gnu-designator", // workaround for compiler error
+    "-fno-omit-frame-pointer",
+    "-gdwarf-2",
+    "-MD",
+    "-ggdb",
+    "-ffreestanding",
+    "-fno-common",
+    "-nostdlib",
+    "-mno-relax",
+    "-fno-pie",
+    "-fno-stack-protector",
+    "-Wno-unused-but-set-variable", // workaround for compiler error
+};
+
+const user_progs = [_][]const u8{
+    // "src/user/forktest.c", // TODO: build forktest
+    "src/user/cat.c",
+    "src/user/echo.c",
+    "src/user/grep.c",
+    "src/user/init.c",
+    "src/user/kill.c",
+    "src/user/ln.c",
+    "src/user/ls.c",
+    "src/user/mkdir.c",
+    "src/user/rm.c",
+    "src/user/sh.c",
+    "src/user/stressfs.c",
+    "src/user/usertests.c",
+    "src/user/grind.c",
+    "src/user/wc.c",
+    "src/user/zombie.c",
+};
+
+const ulib_src = [_][]const u8{
+    "src/user/ulib.c",
+    "src/user/printf.c",
+    "src/user/umalloc.c",
+};
+
+const syscalls = [_][]const u8{
+    "fork", // Create a process, return childΓÇÖs PID.
+    "exit", // Terminate the current process; status reported to wait(). No return.
+    "wait", // Wait for a child to exit; exit status in *status; returns child PID.
+    "pipe", // Create a pipe, put read/write file descriptors in p[0] and p[1].
+    "read", // Read n bytes into buf; returns number read; or 0 if end of file.
+    "write", // Write n bytes from buf to file descriptor fd; returns n.
+    "close", // Release open file fd.
+    "kill", // Terminate process PID. Returns 0, or -1 for error.
+    "exec", // Load a file and execute it with arguments; only returns if error.
+    "open", // Open a file; flags indicate read/write; returns an fd (file descriptor).
+    "mknod", // Create a device file.
+    "unlink", // Remove a file.
+    "fstat", // Place info about an open file into *st.
+    "link", // Create another name (file2) for the file file1.
+    "mkdir", // Create a new directory.
+    "chdir", // Change the current directory.
+    "dup", // Return a new file descriptor referring to the same file as fd.
+    "getpid", // Return the current processors PID.
+    "sbrk", // Grow processors memory by n bytes. Returns start of new memory.
+    "sleep", // Pause for n clock ticks.
+    "uptime",
+};
 
 pub fn build(b: *std.build.Builder) !void {
     const target = std.zig.CrossTarget{
@@ -15,182 +113,85 @@ pub fn build(b: *std.build.Builder) !void {
     const kernel_linker = "scripts/kernel.ld";
     const user_linker = "scripts/user.ld";
 
-    const kernel_src = [_][]const u8{
-        "kernel/entry.S", // Very first boot instructions.
-        "kernel/start.c", // Early machine-mode boot code.
-        "kernel/console.c", // Connect to the user keyboard and screen.
-        "kernel/printf.c", // Formatted output to the console.
-        "kernel/uart.c", // Serial-port console device driver.
-        "kernel/kalloc.c", // Physical page allocator.
-        "kernel/spinlock.c", // Locks that don’t yield the CPU.
-        "kernel/string.c", // C string and byte-array library.
-        "kernel/main.c", // Control initialization of other modules during boot.
-        "kernel/vm.c", // Manage page tables and address spaces.
-        "kernel/proc.c", // Processes and scheduling.
-        "kernel/swtch.S", // Thread switching.
-        "kernel/trampoline.S", // Assembly code to switch between user and kernel.
-        "kernel/trap.c", // C code to handle and return from traps and interrupts.
-        "kernel/syscall.c", // Dispatch system calls to handling function.
-        "kernel/sysproc.c", // Process-related system calls.
-        "kernel/bio.c", // Disk block cache for the file system.
-        "kernel/fs.c", // File system.
-        "kernel/log.c", // File system logging and crash recovery.
-        "kernel/sleeplock.c", // Locks that yield the CPU.
-        "kernel/file.c", // File descriptor support.
-        "kernel/pipe.c", // Pipes.
-        "kernel/exec.c", // exec() system call.
-        "kernel/sysfile.c", // File-related system calls.
-        "kernel/kernelvec.S", // Handle traps from kernel, and timer interrupts.
-        "kernel/plic.c", // RISC-V interrupt controller.
-        "kernel/virtio_disk.c", // Disk device driver.
-    };
-
-    const cflags = [_][]const u8{
-        "-Wall",
-        "-Werror",
-        "-Wno-gnu-designator", // workaround for compiler error
-        "-fno-omit-frame-pointer",
-        "-gdwarf-2",
-        "-MD",
-        "-ggdb",
-        "-ffreestanding",
-        "-fno-common",
-        "-nostdlib",
-        "-mno-relax",
-        "-fno-pie",
-        "-fno-stack-protector",
-        "-Wno-unused-but-set-variable", // workaround for compiler error
-    };
-
     const kernel = b.addExecutable(.{
         .name = "kernel",
         .target = target,
         .optimize = std.builtin.Mode.ReleaseSmall,
     });
     kernel.addCSourceFiles(&kernel_src, &cflags);
-    kernel.addIncludePath(".");
+    kernel.addIncludePath(.{ .path = "src" });
     kernel.setLinkerScriptPath(.{ .path = kernel_linker });
     kernel.code_model = .medium;
-    kernel.install();
     kernel.strip = true;
+    b.installArtifact(kernel);
 
-    const user_progs = [_][]const u8{
-        // "user/forktest.c", // ToDo: build forktest
-        "user/cat.c",
-        "user/echo.c",
-        "user/grep.c",
-        "user/init.c",
-        "user/kill.c",
-        "user/ln.c",
-        "user/ls.c",
-        "user/mkdir.c",
-        "user/rm.c",
-        "user/sh.c",
-        "user/stressfs.c",
-        "user/usertests.c",
-        "user/grind.c",
-        "user/wc.c",
-        "user/zombie.c",
-    };
-
-    const ulib_src = [_][]const u8{
-        "user/ulib.c",
-        "user/usys.S",
-        "user/printf.c",
-        "user/umalloc.c",
-    };
-
-    const usys_install = try generateUsys(b);
+    const syscall_gen_step = addSyscallGen(b, &syscalls);
 
     var artifacts = std.ArrayList(*CompileStep).init(b.allocator);
     inline for (user_progs) |src| {
-        const exe_name = "_" ++ src["user/".len .. src.len - 2];
+        const src_files = &[_][]const u8{src} ++ ulib_src;
+        const exe_name = "_" ++ src["src/user/".len .. src.len - 2];
         const user_prog = b.addExecutable(.{
             .name = exe_name,
             .target = target,
             .optimize = std.builtin.Mode.ReleaseSmall,
         });
-        user_prog.addCSourceFiles(&[_][]const u8{src} ++ ulib_src, &cflags);
-        user_prog.addIncludePath(".");
+        user_prog.addCSourceFiles(src_files, &cflags);
+        user_prog.addCSourceFile(.{
+            .file = syscall_gen_step.getFileSource(),
+            .flags = &cflags,
+        });
+        user_prog.addIncludePath(.{ .path = "src" });
         user_prog.setLinkerScriptPath(.{ .path = user_linker });
         user_prog.code_model = .medium;
-        user_prog.install();
-        user_prog.step.dependOn(&usys_install.step);
+        user_prog.step.dependOn(&syscall_gen_step.step);
+        b.installArtifact(user_prog);
         try artifacts.append(user_prog);
     }
+
     const image = installFilesystem(b, artifacts, "fs.img");
-    _ = qemuRun(b, kernel, image);
-}
-
-pub fn generateUsys(b: *std.build.Builder) !*InstallFileStep {
-    const syscalls = [_][]const u8{
-        "fork", // Create a process, return childΓÇÖs PID.
-        "exit", // Terminate the current process; status reported to wait(). No return.
-        "wait", // Wait for a child to exit; exit status in *status; returns child PID.
-        "pipe", // Create a pipe, put read/write file descriptors in p[0] and p[1].
-        "read", // Read n bytes into buf; returns number read; or 0 if end of file.
-        "write", // Write n bytes from buf to file descriptor fd; returns n.
-        "close", // Release open file fd.
-        "kill", // Terminate process PID. Returns 0, or -1 for error.
-        "exec", // Load a file and execute it with arguments; only returns if error.
-        "open", // Open a file; flags indicate read/write; returns an fd (file descriptor).
-        "mknod", // Create a device file.
-        "unlink", // Remove a file.
-        "fstat", // Place info about an open file into *st.
-        "link", // Create another name (file2) for the file file1.
-        "mkdir", // Create a new directory.
-        "chdir", // Change the current directory.
-        "dup", // Return a new file descriptor referring to the same file as fd.
-        "getpid", // Return the current processΓÇÖs PID.
-        "sbrk", // Grow processΓÇÖs memory by n bytes. Returns start of new memory.
-        "sleep", // Pause for n clock ticks.
-        "uptime",
-    };
-
-    var usys_file = try mem.concat(b.allocator, u8, &[_][]const u8{
-        "# generated by build.zig - do not edit\n",
-        "#include \"kernel/syscall.h\"\n",
-    });
-    defer b.allocator.free(usys_file);
-
-    inline for (syscalls) |syscall| {
-        usys_file = try mem.concat(b.allocator, u8, &[_][]const u8{
-            usys_file,
-            ".global " ++ syscall ++ "\n",
-            syscall ++ ":\n",
-            " li a7, SYS_" ++ syscall ++ "\n",
-            " ecall\n ret\n",
-        });
-    }
-
-    const write_usys = b.addWriteFile("usys.S", usys_file);
-    const usys_install = b.addInstallFile(
-        write_usys.getFileSource("usys.S").?,
-        "../user/usys.S",
-    );
-    usys_install.step.dependOn(&write_usys.step);
-
-    return usys_install;
+    qemuRun(b, kernel, image);
 }
 
 /// Output filesystem image determined by filename
-pub fn installFilesystem(b: *std.build.Builder, artifacts: std.ArrayList(*CompileStep), dest_filename: []const u8) *MakeFilesystemStep {
+pub fn installFilesystem(
+    b: *std.Build,
+    artifacts: std.ArrayList(*CompileStep),
+    dest_filename: []const u8,
+) *MakeFilesystemStep {
     const img = addMakeFilesystem(b, artifacts, dest_filename);
     b.getInstallStep().dependOn(&img.step);
     return img;
 }
 
-pub fn addMakeFilesystem(b: *std.build.Builder, artifacts: std.ArrayList(*CompileStep), dest_filename: []const u8) *MakeFilesystemStep {
+pub fn addMakeFilesystem(
+    b: *std.Build,
+    artifacts: std.ArrayList(*CompileStep),
+    dest_filename: []const u8,
+) *MakeFilesystemStep {
     return MakeFilesystemStep.create(b, artifacts, dest_filename);
 }
 
-/// Run os with qemu
-pub fn qemuRun(b: *std.build.Builder, kernel: *CompileStep, image: *MakeFilesystemStep) *QemuRunStep {
-    const run_step = addQemuRun(b, kernel, image);
-    b.getInstallStep().dependOn(&run_step.step);
-    return run_step;
+pub fn addSyscallGen(
+    b: *std.Build,
+    data: []const []const u8,
+) *SyscallGenStep {
+    return SyscallGenStep.create(b, data);
 }
 
-pub fn addQemuRun(b: *std.build.Builder, kernel: *CompileStep, image: *MakeFilesystemStep) *QemuRunStep {
-    return QemuRunStep.create(b, kernel, image);
+pub fn qemuRun(
+    b: *std.Build,
+    kernel: *CompileStep,
+    image: *MakeFilesystemStep,
+) void {
+    if (!b.enable_qemu) return;
+
+    const run_step = RunStep.create(b, "run xv6 step");
+    b.getInstallStep().dependOn(&run_step.step);
+
+    const qemu_run_step = QemuRunStep.create(b, kernel, .{
+        .image = image,
+        .run_step = run_step,
+    });
+    b.getInstallStep().dependOn(&qemu_run_step.step);
 }
